e6d3e0555c3d8c8ada8715d6aff22479a33cd4fd
tagueteamtrucking-afk <tagueteamtrucking@gmail.com>
2025-08-27 12:42:24 -0400

    Create roster.js

---
+++ asset/js/roster.js
@@ -0,0 +1,17 @@
+export const ROSTER = [
+  { key:'stella',      name:'Stella',      file:'stella.vrm' },
+  { key:'tracy',       name:'Tracy',       file:'tracy.vrm' },
+  { key:'alexandria',  name:'Alexandria',  file:'alexandria.vrm' },
+  { key:'jem',         name:'Jem',         file:'jem.vrm' },
+  { key:'carol',       name:'Carol',       file:'carol.vrm' },
+  { key:'nina',        name:'Nina',        file:'nina.vrm' },
+  { key:'charlotte',   name:'Charlotte',   file:'charlotte.vrm' },
+  { key:'abbey',       name:'Abbey',       file:'abbey.vrm' },
+  { key:'billie',      name:'Billie',      file:'billie.vrm' },
+  { key:'odessa',      name:'Odessa',      file:'odessa.vrm' },
+  { key:'sorcha',      name:'Sorcha',      file:'sorcha.vrm' },
+  { key:'themis',      name:'Themis',      file:'themis.vrm' },
+  { key:'clarice',     name:'Clarice',     file:'clarice.vrm' },
+  { key:'white_star',  name:'White Star',  file:'white_star.vrm' },
+  { key:'rey_czar',    name:'Rey Czar',    file:'rey_czar.vrm' }
+];
